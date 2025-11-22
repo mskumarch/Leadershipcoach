86a2bbbf9ec71a9db42f35ee41f59a435279cd05
mskumarch <mskumar.127@gmail.com>
2025-11-22 17:14:34 -0500

    Delete src directory
    duplicate code

--- src/main/java/com/meetingcoach/leadershipconversationcoach/LeadershipCoachApp.kt
+++
@@ -1,7 +0,0 @@
-package com.meetingcoach.leadershipconversationcoach
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class LeadershipCoachApp : Application()
